0d5dfe17deacefb241fca9fa0c8a05e78440f5bb
Alessandro Lavagnino G <aless.lavagnino@gmail.com>
2022-03-22 22:44:24 -0600

    test: subiendo archivos para prueba

---
+++ Problemas_Programacion/problema2.c
@@ -0,0 +1,16 @@
+/*
+Autor:          aless
+Compilador:     gcc (Ubuntu 9.4.0-1ubuntu1~20.04) 9.4.0
+Compilado:      gcc -o problema2.out problema2.c
+Fecha:          03/18/22
+Librerias:      stdio
+Resumen:        El programa lee un número indeterminado de alturas y termina hasta que se ingrese una negativa.
+                Calcula la media, máxima y mínima de las ingresadas.
+Entrada:        
+Salida:         
+*/
+
+//Librerias
+#include <stdio.h>
+//Numerar los pasos del pseudocódigo.
+
