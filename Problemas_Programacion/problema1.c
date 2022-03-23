eba463038c557627dcd033f534f359c94b8988a7
Alessandro Lavagnino G <aless.lavagnino@gmail.com>
2022-03-23 13:32:05 -0600

    build: Se creó el archivo para el problema1

---
+++ Problemas_Programacion/problema1.c
@@ -0,0 +1,12 @@
+/*
+Autor:          aless
+Fecha:          03/23/22
+Compilador:     gcc (Ubuntu 9.4.0-1ubuntu1~20.04) 9.4.0
+Compilado:      gcc -o problema1.out problema1.c
+Librerias:      stdio
+Resumen:        El programa lee tres números reales ingresados por el usuario y escribe la media aritmética únicamente de los números
+                positivos.
+*/
+
+//Librerias
+#include <stdio.h>
--- Problemas_Programacion/problema2.c
+++ Problemas_Programacion/problema2.c
@@ -1,13 +1,11 @@
 /*
 Autor:          aless
+Fecha:          03/18/22
 Compilador:     gcc (Ubuntu 9.4.0-1ubuntu1~20.04) 9.4.0
 Compilado:      gcc -o problema2.out problema2.c
-Fecha:          03/18/22
 Librerias:      stdio
 Resumen:        El programa lee un número indeterminado de alturas y termina hasta que se ingrese una negativa.
                 Calcula la media, máxima y mínima de las ingresadas.
-Entrada:        
-Salida:         
 */
 
 //Librerias
