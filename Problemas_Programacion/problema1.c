3eb2a9c24814963111d34500e74418592f22b42f
Alessandro Lavagnino G <aless.lavagnino@gmail.com>
2022-03-27 19:18:05 -0600

    build: Se hizo el problema 1

--- Problemas_Programacion/problema1.c
+++ Problemas_Programacion/problema1.c
@@ -2,7 +2,7 @@
 Autor:          aless
 Fecha:          03/23/22
 Compilador:     gcc (Ubuntu 9.4.0-1ubuntu1~20.04) 9.4.0
-Compilado:      gcc -o problema1.out problema1.c
+Compilado:      gcc -o problema1.out problema1.c && ./problema1.out
 Librerias:      stdio
 Resumen:        El programa lee tres números reales ingresados por el usuario y escribe la media aritmética únicamente de los números
                 positivos.
