537cbcd3de84a14ae81f0bc3c1f51cba627d649c
Alessandro Lavagnino G <aless.lavagnino@gmail.com>
2022-03-28 01:57:52 -0600

    build: Se hizo el problema 2, se arregló el readme

--- Problemas_Programacion/README.md
+++ Problemas_Programacion/README.md
@@ -4,16 +4,21 @@ Dentro de esta carpeta se documentaran y se mostrará el código de los problema
 ## Problema 1
 ![Problema1](/2022LabSimu201907400/Problemas_Programacion/Imagenes/P1.png)
 ### Variables de entrada:
-    i = Contador de números ingresados
-    n = Contador de números positivos ingresados.
-    x = Almacena temporalmente el número ingresado.
-    media = Almacena la suma de los números ingresados.
+    i: Contador de números ingresados
+    n: Contador de números positivos ingresados.
+    x: Almacena temporalmente el número ingresado.
+    media: Almacena la suma de los números ingresados.
 ### Compilación y ejecución:
     $ gcc -o problema1.out problema1.c && ./problema1.out
 
 ## Problema 2
 ![Problema2](/2022LabSimu201907400/Problemas_Programacion/Imagenes/P2.png)
 ### Variables de entrada:
+    n: Contador de números ingresados (mayores o iguales a cero)
+    x: Almacena temporalmete el número ingresado.
+    med: Almacena la suma de los números ingresados (mayores o iguales a cero).
+    min: Almacena el número de menor valor.
+    max: Almacena el número de mayor valor.
 ### Compilación y ejecución:
     $ gcc -o problema2.out problema2.c && ./problema2.out
 
--- Problemas_Programacion/problema2.c
+++ Problemas_Programacion/problema2.c
@@ -2,13 +2,42 @@
 Autor:          aless
 Fecha:          03/18/22
 Compilador:     gcc (Ubuntu 9.4.0-1ubuntu1~20.04) 9.4.0
-Compilado:      gcc -o problema2.out problema2.c
+Compilado:      gcc -o problema2.out problema2.c && ./problema2.out
 Librerias:      stdio
 Resumen:        El programa lee un número indeterminado de alturas y termina hasta que se ingrese una negativa.
-                Calcula la media, máxima y mínima de las ingresadas.
+                Luego calcula la media, máxima y mínima de las ingresadas.
 */
 
 //Librerias
 #include <stdio.h>
-//Numerar los pasos del pseudocódigo.
+
+int main() {
+    int n=0;                                    // Inicializamos variables
+    float x=0, med=0, max=0, min=0;
+    puts("Ingrese una altura");
+    scanf("%f", &x);                            // Leemos lo escrito y lo asignamos a la variable x. La cual servirá para almacenar las alturas.
+    max=x;                                      // Asignamos el valor de x a las variables max y min.
+    min=x;
+        while (x>=0) {                          // Si la altura ingresada es mayor o igual a cero, entramos al siguiente ciclo
+            n++;                                // Se suma "1" al contador de números positivos ingresados.
+            med+=x;                             // Se suma el valor de la variable x a la variable med
+            if (x>max) {                        // 
+                max=x;
+            }
+            if (x<min) {
+                min=x;
+            }
+            puts("Ingrese otra altura");
+            scanf("%f", &x);
+        }
+        if (n==0) {
+            puts("No positivos");
+        }
+        else {
+            printf("La media es: %.2f \n", med/n);
+            printf("El valor máximo es: %.2f \n", max);
+            printf("El valor mínimo es: %.2f \n", min);
+        }
+return 0;
+}
 
