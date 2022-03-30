e66d7306a69b1c3bfd928434ba5438e3d9121377
Alessandro Lavagnino G <aless.lavagnino@gmail.com>
2022-03-30 12:32:48 -0600

    fix: pequeña correción al resumen. feat: Se agregó un do while. chore: Se redujo el tamaño del código.

--- Problemas_Programacion/problema3.c
+++ Problemas_Programacion/problema3.c
@@ -4,26 +4,20 @@ Fecha:          03/29/22
 Compilador:     gcc (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0
 Compilado:      gcc -o problema3.out problema3.c && ./problema3.out
 Librerias:      stdio
-Resumen:        Sea (n) un entero mayor a cero. Sea (r) otro entero mayor a cero tal que (r^2 <= n). Este programa calcula (r) dado un (n) ingresado por
-                el usuario.
+Resumen:        Sea (n) un entero mayor a cero. Sea (r) otro entero mayor a cero tal que (r^2 <= n). Dado un (n) ingresado por
+                el usuario, este programa calcula el mayor (r) que cumpla esa condición.
 */
 
 //Librerias
 #include <stdio.h>
 
 int main() {
-    int n=0, r=0;                                                   // 1. Se inicializan las variables
-    puts("Ingrese el número al que le quiere calcular su raíz:");   // 2. Se pide que se ingrese (n) y se lee.
-    scanf("%d", &n);
-    if (n>0) {                                                      // 3. Se valida que (n) sea mayor a cero
-        r=1;
-            while (r*r<=n) {                                        // 4. El ciclo aumenta el valor de (r) hasta que se cumpla la condición.
-                r++;
-            }
-        printf("La raíz aproximada es: %d \n", r-1);                // 5. Cuando se cumpla la condición, se imprime (r-1)
-    }
-    else if (n<1) {                                                 // 6. Si (n) es menor a uno, se termina el programa.
-        puts("El valor ingresado tiene que ser mayor a cero.");
-    }
+int n=0, r=1;                                               // 1. Se inicializan las variables
+    do {                                                    // 2. El usuario entra en un ciclo del cual solo puede salir si se cumple la condición.
+        puts("Ingrese el número al que le quiere calcular su raíz (tiene que ser mayor o igual a 1):");
+        scanf("%d", &n);
+    } while (n<=0);
+    while (r*r<=n) { r++; }                                 // 3. El ciclo aumenta el valor de (r) hasta que se deje de cumplir la condición.
+    printf("La raíz entera aproximada es: %d \n", r-1);     // 4. En dado caso, se imprime (r-1). Que es el mayor número que cumplió la condición.
 return 0;
 }
