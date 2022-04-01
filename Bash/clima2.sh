c2d29dc42a7a6f74e61b8eaf2b5ecfc3e1532dad
Alessandro Lavagnino G <aless.lavagnino@gmail.com>
2022-04-01 11:04:26 -0600

    build: Compactación del ejercicio del clima

---
+++ Bash/clima2.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+: << 'Header'
+Autor:              Alessandro Lavagnino
+Carnet:             201907400
+Fecha:              01/03/2022
+Tipo de archivo:    Script de bash
+Ejecución:          ./clima2.sh
+Resumen:            Obtenemos la información del clima e imprimimos las primeras 7 líneas de esta directamente a la terminal. Saltandonos la creación
+                    del archivo temporal.
+Header
+
+( curl -s wttr.in/Guatemala |  head -n 7 )
