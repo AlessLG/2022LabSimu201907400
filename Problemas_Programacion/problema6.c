ab2c92c687db6959546dcbe4160c80f702442c93
Alessandro Lavagnino G <aless.lavagnino@gmail.com>
2022-04-21 13:41:03 -0600

    chore: Se terminó el problema 6

--- Problemas_Programacion/problema6.c
+++ Problemas_Programacion/problema6.c
@@ -4,33 +4,44 @@ Fecha:          04/11/22
 Compilador:     gcc (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0
 Compilado:      gcc -o problema6.out problema6.c && ./problema6.out
 Librerias:      stdio
-Resumen:        Este programa 
+Resumen:        Este programa calcula el área de un polígono, dada una serie de coordenadas de vértices ingresadas.
 */
 
 #include <stdio.h>              // Librerias
+
 int main() {
-    int FM, CM=2, fila, col;    // Definimos las variables que definirán el tamaño de nuestra matriz. Y dos variables de conteo.
-    do {
-        printf("Escriba el número de vértices que tendrá el polígono. El número debe ser mayor o igual que 3.: ");
+    int FM, N, fila, i;         // Definimos variables.
+    float A;
+    puts("----Calculadora del área de un polígono, dado sus vértices----");
+    do {                        // Pedimos el número de vértices.
+        puts("Escriba el número de vértices que tendrá el polígono. El número debe ser mayor o igual que 3.");
         scanf("%d", &FM);
     } while (FM<3);
-    printf("El número de vértices es: %d\n", FM);
 
-    int matriz[FM][CM];                       // Declaramos la matriz
+    int matriz[FM][2];          // Declaramos la matriz como una de FM filas x 2 columnas.
 
-    for (fila = 0; fila < FM; fila++) {         // Para ingresar los valores a la matriz.
-        for (col = 0; col < CM; col++) {
-            printf("Ingrese un valor: ");
-            scanf("%d", &matriz[fila][col]);
-        }
+    puts("--------------------------------");
+    puts("Ingrese las coordenadas de los vértices del polígono.");
+    puts(" • Separe las entradas de las coordenadas por un espacio o un enter. Y las coordenadas por enters.");
+    puts(" • Deben ser números enteros.");
+    puts(" • Las coordenadas deben de ser ingresadas ordenadamente, de tal forma que vayan en contra de las manecillas del reloj.");
+    puts(" • El cálculo no funciona si el polígono se intersecta a si mismo o si tiene agujeros.");
+    puts("--------------------------------");
+    for (fila = 0; fila < FM; fila++) {                                     // Se pide ingresar los valores a la matriz. Fila por fila,
+        scanf("%d %d", &matriz[fila][0], &matriz[fila][1]);                 // las dos columnas de un solo.
     }
-    puts("Los valores de la matriz son:");
-    for (fila = 0; fila < FM; fila++) {         // Para imprimir los valores a la matriz.
-        for (col = 0; col < CM-1; col++) {
-            printf("%d", matriz[fila][col]);
-            printf("\t%d\n", matriz[fila][col+1]);
-        }
+
+    puts("Las coordenadas de los vértices del polígono son:");
+    for (fila = 0; fila < FM; fila++) {                                     // Se muestran las coordenadas ingresadas.
+        printf("(%d, %d)\n", matriz[fila][0], matriz[fila][1]);
     }
-    puts("");
+
+    N = FM-1;                                                               // Definimos la variable N
+
+    for (i = 0; i < N; i++) {                                               // Hacemos los cálculos que corresponden a la sumatoria.
+        A += (matriz[i][0]*matriz[i+1][1]) - (matriz[i+1][0]*matriz[i][1]);
+    }
+    A += (matriz[N][0]*matriz[0][1])-(matriz[0][0]*matriz[N][1]);           // Hacemos el cálculo que está afuera de la sumatoria.
+    printf("El área del polígono es: %.2f\n", A/2);                         // Mostramos el resultado.
 return 0;
 }
