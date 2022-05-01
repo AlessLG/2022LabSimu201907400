8cfba318b2d1d3e765fede41d8dcc0753c989c49
Alessandro Lavagnino G <aless.lavagnino@gmail.com>
2022-04-30 23:08:57 -0600

    fix: Newton Raphson arreglado y mejorado. Faltan comentarios.

--- Segundo_Parcial/NewtonRaphson.c
+++ Segundo_Parcial/NewtonRaphson.c
@@ -17,59 +17,58 @@ float NewtonRaphson(float x, float e, int N);
 float absoluto(float x);
 
 int main() {
-    float aprox, toler, resultNR;
-    int iter;
-                                                                                        printf("Ingrese una aproximación para la raíz: ");
-                                                                                        scanf("%f", &aprox);
-                                                                                        do {
-                                                                                            printf("Ingrese el valor de tolerancia (debe ser mayor a 0): ");
-                                                                                            scanf("%f", &toler);
-                                                                                        } while (toler <= 0);
-                                                                                        do {
-                                                                                            printf("Ingrese el máximo número de iteraciones que quiere hacer (debe ser mayor o igual a 1): ");
-                                                                                            scanf("%d", &iter);
-                                                                                        } while (iter < 1);
-    resultNR = f(NewtonRaphson(aprox, toler, iter));
-    printf("f(resultNR) = %f, absoluto(f(resultNR)) = %f\n", resultNR, absoluto(resultNR));
-    if (absoluto(resultNR) <= toler) {
-        printf("El resultado es: %f\n", resultNR);
+    float aprox = 0, toler = 0, resultNR = 0;
+    int iter = 0;
+    printf("Ingrese una aproximación para la raíz: ");
+    scanf("%f", &aprox);
+    do {
+        printf("Ingrese el valor de tolerancia (debe ser mayor a 0): ");
+        scanf("%f", &toler);
+    } while (toler <= 0);
+    do {
+        printf("Ingrese el máximo número de iteraciones que quiere hacer (debe ser mayor o igual a 1): ");
+        scanf("%d", &iter);
+    } while (iter < 1);
+
+    resultNR = NewtonRaphson(aprox, toler, iter);
+    //printf("resultNR = %f, f(resultNR) = %f, absoluto(f(resultNR)) = %f\n", resultNR, f(resultNR), absoluto(f(resultNR)));
+    if (absoluto(f(resultNR)) <= toler) {
+        printf("El resultado es:\t%f\n", resultNR);
+        printf("Su error es de:\t\t%f\n", absoluto(f(resultNR)));
     }
-    else {
-        printf("No hay solución luego de %d iteraciones\n", iter);
-    }
+    else
+        printf("No hay solución luego de %d iteraciones.\n", iter);
     return 0;
 }
 
 float NewtonRaphson(float xant, float e, int N) {
-    float x;
-    int i=0;
-    printf("e = %f, N= %d\n", e, N);
-    while (i<N) {
+    float x = 0;
+    int i = 0;
+    // printf("xant = %f, e = %f, N = %d\n", xant, e, N);
+    printf("----------------------------------------------------------------------------------\n");
+    printf("[i]\t[x_i]\t\t[f(x_i)]\t[f'(x_i)]\t[x_i - f(x_i)/f'(x_i)]\n");
+    while (i<N && f(xant)>e) {
+        printf("%d\t%f\t%f\t%f\t%f\n", i+1, xant, f(xant), df(xant), xant-(f(xant)/df(xant)));
         x = xant-f(xant)/df(xant);
         xant = x;
         i++;
-        printf("[%d.] x = %f,\tf(x)=%f\n", i, x, f(x));
     }
+    printf("----------------------------------------------------------------------------------\n");
     return x;
 }
 
 float f(float x) {
-    float res = 0;
-    res = exp(x)+x;
-    return res;
+    x = exp(x)+x;
+    return x;
 }
 
 float df(float x) {
-    float res = 0;
-    res = exp(x)+1;
-    return res;
+    x = exp(x)+1;
+    return x;
 }
 
 float absoluto(float x) {
-    float res = 0;
     if (x<0)
-        res = -x;
-    else if (x>=0)
-        res = res;
-    return res;
+        x = -x;
+    return x;
 }
